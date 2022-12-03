98c507b2435e00d999c735652c3bbb50fef9c45c
Jakub Olszewski <jakubolszewski@tuta.io>
2022-12-03 03:51:50 +0000

    Update void-install.sh

--- void-install.sh
+++ void-install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 curl -Lo /tmp/typora.deb https://gitlab.com/jakubolszewski/TyporaFinalBeta/-/raw/main/typora_0.11.18_amd64.deb
+curl -Lo /tmp/xdeb https://gitlab.com/jakubolszewski/TyporaFinalBeta/-/raw/main/typora_0.11.18_amd64.debtypora.deb
