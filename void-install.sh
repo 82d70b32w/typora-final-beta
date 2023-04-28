6a2aaf7b5ea537ee2b095baaa8b4562b9e88e120
Jakub Olszewski <w23b07d28@gmail.com>
2023-04-28 17:02:57 +0200

    Delete void-install.sh

--- void-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -Lo /tmp/typora.deb https://gitlab.com/jakubolszewski/TyporaFinalBeta/-/raw/main/typora_0.11.18_amd64.deb
-curl -Lo /tmp/xdeb https://gitlab.com/jakubolszewski/TyporaFinalBeta/-/raw/main/typora_0.11.18_amd64.debtypora.deb
